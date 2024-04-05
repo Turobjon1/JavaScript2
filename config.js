5871b3c05c762e8aadd5a749df49705daec02599
Turobjon <avezovzamir337@gmail.com>
2024-04-05 23:21:06 +0500

    first commit

--- config.js
+++ config.js
@@ -1,12 +1,15 @@
-let son1 = Math.random()*100
+let ism = prompt(`String  typedagi malumotn kiriting  `)
 
-let son2 = son1.toFixed()
+let number = prompt(`number  typedagi malumotn kiriting  `)
 
-
-
-
-console.log(son2);
+let boolean = prompt(` boolean  typedagi malumotn kiriting  `)
 
 
+boolean =confirm( `hamma malumotlarni togri kiritganingizga ishonchingiz komilmi`)
 
 
+alert( `siz kiritgan  ${ism} sozi string typega tegishli`  )
+
+alert( `siz kiritgan  ${number} sozi number typega tegishli`)
+
+alert( `siz kiritgan  ${boolean} sozi boolean typega tegishli`)
